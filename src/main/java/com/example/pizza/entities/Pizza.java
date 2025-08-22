f304c79b0434d13df6f815feea0e83e6b2f2e8ed
Jacob Smith <jacob.w.smith97.mil@army.mil>
2025-08-22 10:58:18 -0500

    add pizza

---
+++ src/main/java/com/example/pizza/entities/Pizza.java
@@ -0,0 +1,11 @@
+package com.example.pizza.entities;
+
+public class Pizza {
+
+    private Long id;
+    private String crust;
+    private double price;
+    private String size;
+    private String toppings;
+    private boolean hasCheese;
+}
